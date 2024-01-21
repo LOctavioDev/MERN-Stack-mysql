eac98cb02545f3cc2cf494bc932c8bbae354ef94
LOctavioDev <octaviodevtech@gmail.com>
2024-01-21 13:27:27 -0600

    Add: Endpoint delete

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,12 +1,23 @@
-import { query } from "express";
+import { json, query } from "express";
 import { pool } from "../db.js";
 
-export const getTasks = (req, res) => {
-  res.send("obteniendo tareas");
+export const getTasks = async (req, res) => {
+  const [result] = await pool.query(
+    "SELECT * FROM tbb_task ORDER BY createAt ASC"
+  );
+
+  res.json(result);
 };
 
-export const getTask = (req, res) => {
-  res.send("obteniendo una tarea");
+export const getTask = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM tbb_task WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if (result.length === 0)
+    return res.status(404).json({ message: "Task not found" });
+
+  res.json(result[0]);
 };
 
 export const createTask = async (req, res) => {
@@ -27,6 +38,14 @@ export const updateTask = (req, res) => {
   res.send("actualizando tarea");
 };
 
-export const delteTask = (req, res) => {
-  res.send("eliminando tareas");
+export const delteTask = async (req, res) => {
+  const [result] = await pool.query("DELETE FROM tbb_task WHERE id = ?", [
+    req.params.id,
+  ]);
+
+  if(result.affectedRows === 0){
+    return res.status(404).json({message: "Task not found"})
+  }
+
+  return res.sendStatus(204)
 };
