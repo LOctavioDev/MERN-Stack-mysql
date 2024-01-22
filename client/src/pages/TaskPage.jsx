ba0cb05e7c0a80bcfa2cce4a5e6184a5d85e2b74
LOctavioDev <octaviodevtech@gmail.com>
2024-01-21 18:56:32 -0600

    Add: Deleted task Front

--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -17,12 +17,15 @@ function TaskPage() {
     loadTasks();
   }, []);
 
+  function renderMain() {
+    if(tasks.length === 0) return <h1>No tasks yet</h1>
+    return tasks.map(task => <TaskCard task={task} key={task.id} />);
+  }
+
   return (
     <div>
       <h1>Tasks</h1>
-      {tasks.map(task => (
-        <TaskCard task={task} key={task.id} />
-      ))}
+      {renderMain()}
     </div>
   );
 }
